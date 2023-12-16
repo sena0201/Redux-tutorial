b420019a3d3550a9b53e3db3324b57913a9d559f
Minh Kha <mnkha02@gmail.com>
2023-12-16 21:06:18 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+import Navbar from "./components/Navbar";
+import { getCartItems } from "./features/cart/cartSlice";
+
+function App() {
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+  const { isOpen } = useSelector((state) => state.modal);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
